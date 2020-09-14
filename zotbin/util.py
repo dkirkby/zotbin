97deba0e8424649f9a78018847983307d2248b30
David Kirkby <dkirkby@uci.edu>
2020-09-14 09:04:23 -0700

    wip

--- zotbin/util.py
+++ zotbin/util.py
@@ -22,6 +22,38 @@ def prepare(data, bands, useflux='i'):
 
 
 def get_signature(X):
-    # Build a ~unique signature for the input dataset.
+    """Build a ~unique signature for the numpy array X suitable for cache file names.
+    """
     signature = np.concatenate((X.shape, np.argmin(X, axis=0), np.argmax(X, axis=0)))
     return hashlib.sha256(signature).hexdigest()
+
+
+def get_file(name, destdir='.',
+             url='https://portal.nersc.gov/cfs/lsst/dkirkby/zotbin/',
+             nersc_path='/global/cfs/cdirs/lsst/www/dkirkby/zotbin',
+             progress_report_frac=0.1):
+    """Locate the named file in the destination directory.
+
+    If necessary, download via HTTP or, if running at NERSC, make a softlink.
+    """
+    dest = os.path.join(destdir, name)
+    if not os.path.exists(dest):
+        if os.environ.get('NERSC_HOST'):
+            src = os.path.join(nersc_path, name)
+            if not os.path.exists(src):
+                raise ValueError(f'File not found at NERSC: {src}')
+            os.symlink(src, dest)
+            print(f'Created a softlink to {src}')
+        else:
+            src = os.path.join(url, name)
+            print(f'Downloading {src}...')
+            next_report_frac = progress_report_frac
+            def progress(block_num, block_size, total_size):
+                nonlocal next_report_frac
+                frac = block_num * block_size / total_size
+                if frac >= next_report_frac:
+                    print(f'...downloaded {100 * frac:.0f}%', flush=True)
+                    next_report_frac += progress_report_frac
+            urlretrieve(src, dest, reporthook=progress)
+    assert os.path.exists(dest)
+    return dest
